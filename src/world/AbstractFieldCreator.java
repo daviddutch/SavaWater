08cff782fa8a731c5303c3acb6951f2f47739ec2
Vincent Ischi <vincent.ischi@gmail.com>
2010-12-06 11:13:13 +0100

    singleton + empty creation

--- src/world/AbstractFieldCreator.java
+++ src/world/AbstractFieldCreator.java
@@ -6,4 +6,5 @@ import factory.AbstractElement;
 
 public interface AbstractFieldCreator {
   public AbstractField createWorld(ArrayList<AbstractElement> elemList);
+  public AbstractField createWorld();
 }
