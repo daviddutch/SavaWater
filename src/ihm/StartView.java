ac61bbd0e38602bd29a1e1d546adb406dc383777
Furet <Furet@160.98.61.180>
2010-12-06 10:34:29 +0100

    Creation startView

---
+++ src/ihm/StartView.java
@@ -0,0 +1,13 @@
+package ihm;
+
+import javax.swing.JFrame;
+
+import system.SystemControl;
+
+public class StartView extends JFrame{
+	private static final long serialVersionUID = 1L;
+	public StartView(SystemControl ctrl){
+		setSize(200,400);
+		
+	}
+}
