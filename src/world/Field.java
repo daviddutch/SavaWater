61c692b05faab2b861a91d15cdbaaf892a8dc99a
Vincent Ischi <vincent.ischi@gmail.com>
2010-12-06 11:13:25 +0100

    singleton

--- src/world/Field.java
+++ src/world/Field.java
@@ -5,7 +5,25 @@ import java.util.ArrayList;
 import factory.AbstractElement;
 
 public class Field extends AbstractField {
-  public Field(ArrayList<AbstractElement> elemList){
+  private static AbstractField field;
+  
+  private Field(ArrayList<AbstractElement> elemList){
+    this.elemList = elemList;
     
+  }
+  
+  public static AbstractField getInstance(ArrayList<AbstractElement> elemList){
+    if(field==null){
+      field = new Field(elemList);
+    }
+    
+    return field;
+  }
+  public static AbstractField getInstance(){
+    if(field==null){
+      field = new Field(new ArrayList<AbstractElement>());
+    }
+    
+    return field;
   }
 }
