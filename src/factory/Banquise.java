4274298f73878495683647be70e38838f92324c3
Furet <Furet@160.98.61.187>
2010-12-22 13:29:22 +0100

    nothing

--- src/factory/Banquise.java
+++ src/factory/Banquise.java
@@ -12,26 +12,11 @@ public class Banquise extends AbstractElementWater {
   }
   @Override
   public void move() {
-    // TODO Auto-generated method stub
+    //Do not move
   }
 
   @Override
   public void evolve() {
-    // TODO Auto-generated method stub
-    
+	  
   }
-
-  /*
-  @Override
-  public GridPoint getPosition() {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
-  @Override
-  public void setPosition(GridPoint point) {
-    // TODO Auto-generated method stub
-    
-  }*/
-
 }
